f4cb180c9a44bc9f194f155f2990326a697c0367
Coffe-Art <CoffeArt224@gmail.com>
2024-08-07 15:12:58 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ app.use(cors({
 // Middleware para agregar la cabecera Access-Control-Allow-Origin
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
